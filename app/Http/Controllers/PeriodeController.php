b5953abac18fc173181e67ead409d9132d61744a
tyagi <79263901+Jinsu-beep@users.noreply.github.com>
2021-07-25 21:06:20 +0800

    Periode
    perbaiki model dan controller periode

--- app/Http/Controllers/PeriodeController.php
+++ app/Http/Controllers/PeriodeController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\PeriodeModel;
 
 class PeriodeController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function dataPeriode()
+    {
+        $periodes = PeriodeModel::get();
+
+        $tperiode = PeriodeModel::orderby('created_at', 'desc')->get();
+
+        return view("Admin.Periode.show", compact('tperiode', 'periodes'));
+    }
 }
