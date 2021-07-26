f46293cfb2f17273a4ba0ad633625b9db4f08bb5
tyagi <79263901+Jinsu-beep@users.noreply.github.com>
2021-07-26 09:41:31 +0800

    periode
    controoler dan route

--- app/Http/Controllers/PeriodeController.php
+++ app/Http/Controllers/PeriodeController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+<<<<<<< Updated upstream
+
+class PeriodeController extends Controller
+{
+    //
+=======
 use App\PeriodeModel;
+use Illuminate\Support\Facades\Validator;
 
 class PeriodeController extends Controller
 {
@@ -20,4 +27,28 @@ class PeriodeController extends Controller
 
         return view("Admin.Periode.show", compact('tperiode', 'periodes'));
     }
+
+    public function addPeriode()
+    {
+        return view('Admin.Periode.create');
+    }
+
+    public function insertPeriode(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id_opd' => 'required',
+            'nama' => 'required',
+            'tgl_mulai' => 'required',
+            'tgl_selesai' => 'required',
+            'status' => 'required',
+            'keterangan' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return back()->withErrors($validator);
+        }
+
+        return redirect('/periode')->with('StatusInput', 'Input Success');
+    }
+>>>>>>> Stashed changes
 }
