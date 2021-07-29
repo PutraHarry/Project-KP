cd2da1fe7e9a1b2ac34a5a1b7a7e54028a9784ad
PutraHarry <56532611+PutraHarry@users.noreply.github.com>
2021-07-29 10:41:56 +0800

    minor changes
    di tampilan login

--- resources/views/Admin/Login.blade.php
+++ resources/views/Admin/Login.blade.php
@@ -17,15 +17,6 @@
   <link rel="stylesheet" href="/adminlte/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-<<<<<<< Updated upstream
-
-  <style>
-    body {
-      background-image: url('adminlte/dist/img/LOGO KAB BADUNG.jpg');
-    }
-    </style>
-
-=======
   
   <!--<style>
     body {
@@ -34,7 +25,6 @@
     }
   </style>-->
   
->>>>>>> Stashed changes
   <!-- jQuery -->
     <script src="/adminlte/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
