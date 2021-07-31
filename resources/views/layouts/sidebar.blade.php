b7a3a6e0ef186a0f7ac71954e526f6ef9bd5d7c0
PutraHarry <56532611+PutraHarry@users.noreply.github.com>
2021-07-31 18:16:57 +0800

    Coba layout tab
    Layout tab dengan tabel di penerimaan

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -82,6 +82,14 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="/penerimaan" class="nav-link">
+              <i class="nav-icon fas fa-chart-bar"></i>
+              <p>
+                Penerimaan
+              </p>
+            </a>
+          </li>
 
           <li class="nav-item">
             <a href="#" class="nav-link">
