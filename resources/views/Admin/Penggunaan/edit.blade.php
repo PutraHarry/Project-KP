972d637bf4e6fd0d70d644432f7de9de76357336
PutraHarry <56532611+PutraHarry@users.noreply.github.com>
2021-10-19 12:32:46 +0800

    Tambahan kode penggunaan

--- resources/views/Admin/Penggunaan/edit.blade.php
+++ resources/views/Admin/Penggunaan/edit.blade.php
@@ -87,9 +87,9 @@ Edit Penggunaan
                     <div class="row">
                         <div class="col-3">
                             <div class="form-group">
-                                <label>Tanggal Penggunaan Barang:</label>
+                                <label>Kode Penggunaan Barang:</label>
                                 <div class="input-group">
-                                    <input type="date" class="form-control" name="tgl_input" id="tgl_input" value="{{ $tpenggunaan->tgl_penggunaan }}">
+                                    <input type="text" class="form-control" name="kode_penggunaan" id="kode_penggunaan" value="{{ $tpenggunaan->kode_penggunaan }}">
                                 </div>  
                             </div>
                         </div>
@@ -111,7 +111,7 @@ Edit Penggunaan
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-6">
+                        <div class="col-3">
                             <div class="form-group">
                                 <label>Kode Penerimaan</label>
                                 <select class="select2" name="id_penerimaan" id="id_penerimaan" data-placeholder="Pilih Nota Bukti Umum" style="width: 100%;">
@@ -119,6 +119,14 @@ Edit Penggunaan
                                     <option value={{ $tp->id }} @if($tp->id == $tpenggunaan->id_penerimaan) selected @endif>{{ $tp->kode_penerimaan }}</option>
                                   @endforeach
                                 </select>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="form-group">
+                                <label>Tanggal Penggunaan Barang:</label>
+                                <div class="input-group">
+                                    <input type="date" class="form-control" name="tgl_input" id="tgl_input" value="{{ $tpenggunaan->tgl_penggunaan }}">
+                                </div>  
                             </div>
                         </div>
                         <div class="col-3">
