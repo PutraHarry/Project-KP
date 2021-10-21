dafe3638892157f9d855239a60a74314fdad8fdb
PutraHarry <56532611+PutraHarry@users.noreply.github.com>
2021-10-21 11:21:46 +0800

    Edit layout penerimaan, pengeluaran dan penggunaan
    nambahin kode penerimaan di penggunaan, nambah kode penggunaan di pengeluaran. nambah koolom keterangan di penerimaan dan penggunaan

--- resources/views/Admin/Penggunaan/create.blade.php
+++ resources/views/Admin/Penggunaan/create.blade.php
@@ -112,6 +112,10 @@ Create Penggunaan Baru
                                   @endforeach
                                 </select>
                             </div>
+                            <div class="form-group">
+                              <label>Keterangan</label>
+                              <textarea class="form-control" rows="3" name="ket_penerimaan" id="ket_penerimaan" placeholder="Input Keterangan..."></textarea>
+                            </div>
                         </div>
                         <div class="col-3">
                             <div class="form-group">
