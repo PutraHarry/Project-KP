6ce30d19d0d64432cf754af5ae68946c8c97ae0c
PutraHarry <56532611+PutraHarry@users.noreply.github.com>
2021-12-12 14:16:21 +0800

    Update BarangController.php

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use App\PeriodeModel;
 use App\BarangModel;
 
